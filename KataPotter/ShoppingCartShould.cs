ed3f58df8f7462f760f5fe8e22e0fc045c375e5d
Suui <suui.gd@gmail.com>
2016-10-03 02:01:47 +0200

    Green: have_a_total_price_of_zero_when_it_is_empty

---
+++ KataPotter/ShoppingCartShould.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+/* TODO
+	
+*/
+
+namespace KataPotter
+{
+	[TestFixture]
+	class ShoppingCartShould
+	{
+		[Test]
+		public void have_a_total_price_of_zero_when_it_is_empty()
+		{
+			var shoppingCart = new ShoppingCart();
+
+			shoppingCart.TotalPrice().Should().Be(1);
+		}
+	}
+
+	public class ShoppingCart
+	{
+		public decimal TotalPrice()
+		{
+			return 1;
+		}
+	}
+}
