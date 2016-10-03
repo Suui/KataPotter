8e2f4f73ea2b1742902817c7e477088bd2a76f13
Suui <suui.gd@gmail.com>
2016-10-03 02:15:47 +0200

    Green: have_a_five_percent_discount_for_two_different_books_of_the_series

--- KataPotter/ShoppingCartShould.cs
+++ KataPotter/ShoppingCartShould.cs
@@ -1,4 +1,6 @@
-﻿using FluentAssertions;
+﻿using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
 using NUnit.Framework;
 
 /* TODO
@@ -17,13 +19,50 @@ namespace KataPotter
 
 			shoppingCart.TotalPrice().Should().Be(0);
 		}
+
+		[Test]
+		public void have_a_price_of_eight_for_a_single_book()
+		{
+			var shoppingCart = new ShoppingCart();
+			shoppingCart.Add(new Book("A book"));
+
+			shoppingCart.TotalPrice().Should().Be(8);
+		}
+
+		[Test]
+		public void have_a_five_percent_discount_for_two_different_books_of_the_series()
+		{
+			var shoppingCart = new ShoppingCart();
+			shoppingCart.Add(new Book("A Book"));
+			shoppingCart.Add(new Book("A Different Book"));
+
+			shoppingCart.TotalPrice().Should().Be(16 * 0.95m);
+		}
 	}
 
 	public class ShoppingCart
 	{
+		private List<Book> Books { get; } = new List<Book>();
+
 		public decimal TotalPrice()
 		{
-			return 0;
+			var price = Books.Count * 8;
+			if (Books.Count == 2) return price * 0.95m;
+
+			return price;
+		}
+
+		public void Add(Book book)
+		{
+			Books.Add(book);
+		}
+	}
+
+	public class Book
+	{
+		public Book(string title)
+		{
+			
 		}
 	}
 }
